64fc41a77502899b4fa9579bd897210b9c3d6ed7
Snapky06 <saidnapky@gmail.com>
2025-08-08 11:58:23 -0600

    finalizado

--- src/sudokulab3progra2_said_samuel/ModeloSudoku.java
+++ src/sudokulab3progra2_said_samuel/ModeloSudoku.java
@@ -1,20 +1,44 @@
 package sudokulab3progra2_said_samuel;
 
-
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Random;
+import java.util.List;
+import javax.swing.JOptionPane;  
+import sudokulab3progra2_said_samuel.Sudoku;
 
 public class ModeloSudoku {
     private int[][] tablero = new int[9][9];
+    private int[][] solucion = new int[9][9];  
     private boolean[][] fijos = new boolean[9][9];
+    private int errores = 0;             
 
     public int obtener(int f, int c) { return tablero[f][c]; }
     public boolean esFijo(int f, int c) { return fijos[f][c]; }
-    public void asignar(int f, int c, int v) { tablero[f][c] = v; }
+    public int getErrores() { return errores; }
+    public void resetErrores() { errores = 0; }
+
+ 
+    public boolean asignar(int f, int c, int v) {
+        if (fijos[f][c]) {
+            errores++;
+            JOptionPane.showMessageDialog(null, "No puedes cambiar una casilla fija.", "Aviso", JOptionPane.WARNING_MESSAGE);
+            return false;
+        }
+        if (v != solucion[f][c]) {
+            errores++;
+            JOptionPane.showMessageDialog(null, "Número incorrecto. Intenta otro.", "Error", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }
+        tablero[f][c] = v;
+        return true;
+    }
 
     public void generar(int pistas) {
+        resetErrores();
         llenarTableroCompleto();
+        for (int i = 0; i < 9; i++) {
+            System.arraycopy(tablero[i], 0, solucion[i], 0, 9);
+        }
         quitarNumeros(81 - pistas);
         for (int f = 0; f < 9; f++) {
             for (int c = 0; c < 9; c++) {
@@ -52,7 +76,7 @@ public class ModeloSudoku {
     private boolean rellenarRecursivo(int f, int c) {
         if (f == 9) return true;
         int nf = (c == 8 ? f + 1 : f), nc = (c == 8 ? 0 : c + 1);
-        ArrayList<Integer> nums = new ArrayList<>();
+        List<Integer> nums = new ArrayList<>();
         for (int i = 1; i <= 9; i++) nums.add(i);
         Collections.shuffle(nums);
         for (int v : nums) {
@@ -66,57 +90,66 @@ public class ModeloSudoku {
     }
 
     private void quitarNumeros(int cuenta) {
-        int eliminado = 0, intentos = 0;
-        Random rnd = new Random();
-        while (eliminado < cuenta && intentos < cuenta * 5) {
-            int f = rnd.nextInt(9), c = rnd.nextInt(9);
-            if (tablero[f][c] == 0) { intentos++; continue; }
-            int[][] copia = copiarTablero();
-            copia[f][c] = 0;
-            if (contarSoluciones(copia, 2) == 1) {
-                tablero[f][c] = 0;
-                eliminado++;
-            }
-            intentos++;
-        }
-    }
-
-    private int contarSoluciones(int[][] bt, int lim) {
-        return contarRecursivo(bt, 0, 0, lim);
-    }
-
-    private int contarRecursivo(int[][] bt, int f, int c, int lim) {
-        if (f == 9) return 1;
-        int nf = (c == 8 ? f + 1 : f), nc = (c == 8 ? 0 : c + 1);
-        if (bt[f][c] != 0) return contarRecursivo(bt, nf, nc, lim);
-        int cnt = 0;
-        for (int v = 1; v <= 9; v++) {
-            if (validoEn(bt, f, c, v)) {
-                bt[f][c] = v;
-                cnt += contarRecursivo(bt, nf, nc, lim);
-                if (cnt >= lim) break;
+        List<int[]> cells = new ArrayList<>();
+        for (int r = 0; r < 9; r++) {
+            for (int c = 0; c < 9; c++) {
+                cells.add(new int[]{r, c});
             }
         }
-        bt[f][c] = 0;
-        return cnt;
+        Collections.shuffle(cells);
+        int removed = 0;
+        for (int[] cell : cells) {
+            if (removed >= cuenta) break;
+            int r = cell[0], c = cell[1];
+            int backup = tablero[r][c];
+            if (backup == 0) continue;
+            tablero[r][c] = 0;
+            int[][] copy = new int[9][9];
+            for (int i = 0; i < 9; i++) System.arraycopy(tablero[i], 0, copy[i], 0, 9);
+            if (esSoluble(copy) && tieneCandidatos(copy)) {
+                removed++;
+            } else {
+                tablero[r][c] = backup;
+            }
+        }
     }
 
-    private boolean validoEn(int[][] bt, int f, int c, int v) {
-        for (int i = 0; i < 9; i++) {
-            if (bt[f][i] == v || bt[i][c] == v) return false;
-        }
-        int sr = (f / 3) * 3, sc = (c / 3) * 3;
-        for (int i = sr; i < sr + 3; i++) {
-            for (int j = sc; j < sc + 3; j++) {
-                if (bt[i][j] == v) return false;
+    private boolean esSoluble(int[][] puzzle) {
+        int[][] copy = new int[9][9];
+        for (int i = 0; i < 9; i++) System.arraycopy(puzzle[i], 0, copy[i], 0, 9);
+        Sudoku solver = new Sudoku(copy);
+        return solver.solve();
+    }
+
+    private boolean tieneCandidatos(int[][] puzzle) {
+        for (int f = 0; f < 9; f++) {
+            for (int c = 0; c < 9; c++) {
+                if (puzzle[f][c] == 0) {
+                    boolean ok = false;
+                    for (int v = 1; v <= 9; v++) {
+                        if (esValidoEnTablero(puzzle, f, c, v)) {
+                            ok = true;
+                            break;
+                        }
+                    }
+                    if (!ok) return false;
+                }
             }
         }
         return true;
     }
 
-    private int[][] copiarTablero() {
-        int[][] copia = new int[9][9];
-        for (int i = 0; i < 9; i++) System.arraycopy(tablero[i], 0, copia[i], 0, 9);
-        return copia;
+    private boolean esValidoEnTablero(int[][] puzzle, int f, int c, int v) {
+        for (int i = 0; i < 9; i++) {
+            if (puzzle[f][i] == v || puzzle[i][c] == v) return false;
+        }
+        int sr = (f / 3) * 3, sc = (c / 3) * 3;
+        for (int i = sr; i < sr + 3; i++) {
+            for (int j = sc; j < sc + 3; j++) {
+                if (puzzle[i][j] == v) return false;
+            }
+        }
+        return true;
     }
 }
+
