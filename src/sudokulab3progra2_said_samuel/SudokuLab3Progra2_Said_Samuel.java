1a7de05a594d18e6f59defa41d4cfbade5a7f791
Samuel Vasquez <83726280+Draccoo222@users.noreply.github.com>
2025-08-08 10:58:53 -0600

    otravez

---
+++ src/sudokulab3progra2_said_samuel/SudokuLab3Progra2_Said_Samuel.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sudokulab3progra2_said_samuel;
+
+/**
+ *
+ * @author unwir
+ */
+public class SudokuLab3Progra2_Said_Samuel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
